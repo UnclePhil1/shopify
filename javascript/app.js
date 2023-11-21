709bb98be510fd880f77b2118f576624bf301ec9
Unclephil <philipnssien@gmail.com>
2023-11-21 14:19:17 +0100

    done with drop for small screen

--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,16 @@
 const dropdownToggle = document.getElementById('dropdownButton');
 const dropdownMenu = document.querySelector('.dropdown-menu');
-const dropdownMenuphoneToggle = document.getElementById('dropdown-toggle-second');
+const dropdownMenuphoneToggle = document.getElementById('dropdownTogglemobile');
+const dropdownMenuMobile = document.querySelector('.dropdownMenuMobile');
 const toggleNotice = document.getElementById('toggleNotice');
 const dropDownNotice = document.querySelector('.noticeBox-dropDown');
 
 toggleNotice.addEventListener('click', () => {
   if (dropDownNotice.style.display === '' || dropDownNotice.style.display === 'none') {
     dropDownNotice.style.display = 'block';
-    dropdownMenu.style.display = 'none'; // Close dropdownMenu if open
+    dropdownMenu.style.display = 'none';
+    dropdownMenuMobile.style.display = 'none';
+     // Close dropdownMenu if open
   } else {
     dropDownNotice.style.display = 'none';
   }
@@ -23,11 +26,11 @@ dropdownToggle.addEventListener('click', () => {
 });
 
 dropdownMenuphoneToggle.addEventListener('click', () => {
-  if (dropdownMenu.style.display === '' || dropdownMenu.style.display === 'none') {
-    dropdownMenu.style.display = 'block';
+  if (dropdownMenuMobile.style.display === '' || dropdownMenuMobile.style.display === 'none') {
+    dropdownMenuMobile.style.display = 'block';
     dropDownNotice.style.display = 'none'; // Close dropDownNotice if open
   } else {
-    dropdownMenu.style.display = 'none';
+    dropdownMenuMobile.style.display = 'none';
   }
 });
 
@@ -35,10 +38,13 @@ window.addEventListener('click', (event) => {
   if (
     !event.target.closest('.dropDown') &&
     !event.target.closest('.dropdown-menu') &&
+    !event.target.closest('.dropdownMenuMobile') &&
     !event.target.closest('.noticeBox-dropDown') &&
+    !event.target.closest('.profileBtn') &&
     !event.target.closest('.notifyButton')
   ) {
     dropdownMenu.style.display = 'none';
     dropDownNotice.style.display = 'none';
+    dropdownMenuMobile.style.display = 'none';
   }
 });
