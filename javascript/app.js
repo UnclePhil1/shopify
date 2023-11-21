438a6232bfeab26b2772b5adba9adac6ed0cd4fe
Unclephil <philipnssien@gmail.com>
2023-11-21 14:07:21 +0100

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,44 @@
+const dropdownToggle = document.getElementById('dropdownButton');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+const dropdownMenuphoneToggle = document.getElementById('dropdown-toggle-second');
+const toggleNotice = document.getElementById('toggleNotice');
+const dropDownNotice = document.querySelector('.noticeBox-dropDown');
+
+toggleNotice.addEventListener('click', () => {
+  if (dropDownNotice.style.display === '' || dropDownNotice.style.display === 'none') {
+    dropDownNotice.style.display = 'block';
+    dropdownMenu.style.display = 'none'; // Close dropdownMenu if open
+  } else {
+    dropDownNotice.style.display = 'none';
+  }
+});
+
+dropdownToggle.addEventListener('click', () => {
+  if (dropdownMenu.style.display === '' || dropdownMenu.style.display === 'none') {
+    dropdownMenu.style.display = 'block';
+    dropDownNotice.style.display = 'none'; // Close dropDownNotice if open
+  } else {
+    dropdownMenu.style.display = 'none';
+  }
+});
+
+dropdownMenuphoneToggle.addEventListener('click', () => {
+  if (dropdownMenu.style.display === '' || dropdownMenu.style.display === 'none') {
+    dropdownMenu.style.display = 'block';
+    dropDownNotice.style.display = 'none'; // Close dropDownNotice if open
+  } else {
+    dropdownMenu.style.display = 'none';
+  }
+});
+
+window.addEventListener('click', (event) => {
+  if (
+    !event.target.closest('.dropDown') &&
+    !event.target.closest('.dropdown-menu') &&
+    !event.target.closest('.noticeBox-dropDown') &&
+    !event.target.closest('.notifyButton')
+  ) {
+    dropdownMenu.style.display = 'none';
+    dropDownNotice.style.display = 'none';
+  }
+});
